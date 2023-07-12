609417ec43ff0d13625e7b4be87e9a99e281610c
ralu87 <139405444+ralu87@users.noreply.github.com>
2023-07-13 06:51:05 +0800

    test change
    test change

---
+++ test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# import requests
+#
+# with open('./txt/abc.txt', 'r') as c1:
+#     content =c1.read()
+#     C = content.strip('')
+#     print(C)
+
+
+d = {'x': 'A', 'y': 'B', 'z': 'C' }
+print(d.items())
